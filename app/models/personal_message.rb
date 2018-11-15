3552d2f6d28f9dfb4b08a18e1bc601b3aeae22f3
Daniel Teale <djteale90@gmail.com>
2018-11-15 16:01:33 +1100

    fixes

--- app/models/personal_message.rb
+++ app/models/personal_message.rb
@@ -4,6 +4,7 @@ class PersonalMessage < ApplicationRecord
   validates :body, presence: true
 
   after_create_commit do
+    conversation.touch
     NotificationBroadcastJob.perform_later(self)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,23 @@
 
 ActiveRecord::Schema.define(version: 2018_11_15_030235) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "conversations", force: :cascade do |t|
     t.integer "author_id"
     t.integer "receiver_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id", "receiver_id"], name: "index_conversations_on_author_id_and_receiver_id", unique: true
     t.index ["author_id"], name: "index_conversations_on_author_id"
     t.index ["receiver_id"], name: "index_conversations_on_receiver_id"
   end
 
   create_table "personal_messages", force: :cascade do |t|
     t.text "body"
-    t.integer "conversation_id"
-    t.integer "user_id"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_personal_messages_on_conversation_id"
@@ -43,4 +47,6 @@ ActiveRecord::Schema.define(version: 2018_11_15_030235) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "personal_messages", "conversations"
+  add_foreign_key "personal_messages", "users"
 end
